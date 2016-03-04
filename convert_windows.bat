f0e6627e5da8e7b3ffdd283a357cc479c23d408e
Jelte Jansen <github@tjeb.nl>
2016-03-04 18:23:35 +0100

    initial addition of v1.2 and peppol rules

--- convert_windows.bat
+++ convert_windows.bat
@@ -1,5 +1,5 @@
 set file_name=SI-UBL-INV
 set convert_base=.
-java -jar ./lib/saxon.jar -o %convert_base%/temp/%file_name%_step_dsdl.xsl %file_name%.SCH %convert_base%/convert/iso_dsdl_include.xsl 
-java -jar ./lib/saxon.jar -o %convert_base%/temp/%file_name%_step_abstract.xsl %convert_base%/temp/%file_name%_step_dsdl.xsl %convert_base%/convert/iso_abstract_expand.xsl
-java -jar ./lib/saxon.jar -o %file_name%-1.1.1.xsl %convert_base%/temp/%file_name%_step_abstract.xsl %convert_base%/convert/iso_svrl_for_xslt1.xsl
+java -jar ./lib/saxon.jar -o %convert_base%/temp/%file_name%_step_dsdl.xsl %file_name%.SCH %convert_base%/convert/iso_dsdl_include_xslt2.xsl 
+java -jar ./lib/saxon.jar -o %convert_base%/temp/%file_name%_step_abstract.xsl %convert_base%/temp/%file_name%_step_dsdl.xsl %convert_base%/convert/iso_abstract_expand_xslt2.xsl
+java -jar ./lib/saxon.jar -o %file_name%.xsl %convert_base%/temp/%file_name%_step_abstract.xsl %convert_base%/convert/iso_svrl_for_xslt2.xsl
