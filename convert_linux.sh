c7be16cc674d6bc0ea1b2316a97bac546ed77554
Jelte Jansen <github@tjeb.nl>
2016-03-04 18:23:35 +0100

    initial addition of v1.2 and peppol rules

--- convert_linux.sh
+++ convert_linux.sh
@@ -15,6 +15,6 @@ function do_cmd {
     $@
 }
 
-do_cmd java -jar ./lib/saxon.jar -o ${CONVERT_BASE}/temp/${FILE_NAME}_step_dsdl.xsl ${FILE_NAME}.SCH ${CONVERT_BASE}/convert/iso_dsdl_include.xsl
-do_cmd java -jar ./lib/saxon.jar -o ${CONVERT_BASE}/temp/${FILE_NAME}_step_abstract.xsl ${CONVERT_BASE}/temp/${FILE_NAME}_step_dsdl.xsl ${CONVERT_BASE}/convert/iso_abstract_expand.xsl
-do_cmd java -jar ./lib/saxon.jar -o ${OUTPUT_FILE} ${CONVERT_BASE}/temp/${FILE_NAME}_step_abstract.xsl ${CONVERT_BASE}/convert/iso_svrl_for_xslt1.xsl
+do_cmd java -jar ./lib/saxon9he.jar -o:${CONVERT_BASE}/temp/${FILE_NAME}_step_dsdl.xsl ${FILE_NAME}.SCH ${CONVERT_BASE}/convert/iso_dsdl_include_xslt2.xsl
+do_cmd java -jar ./lib/saxon9he.jar -o:${CONVERT_BASE}/temp/${FILE_NAME}_step_abstract.xsl ${CONVERT_BASE}/temp/${FILE_NAME}_step_dsdl.xsl ${CONVERT_BASE}/convert/iso_abstract_expand_xslt2.xsl
+do_cmd java -jar ./lib/saxon9he.jar -o:${OUTPUT_FILE} ${CONVERT_BASE}/temp/${FILE_NAME}_step_abstract.xsl ${CONVERT_BASE}/convert/iso_svrl_for_xslt2.xsl
