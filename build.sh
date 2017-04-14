3716f8e6f834730c080530e5a0c140910cc8df13
Rik Ribbers <rik.ribbers@gmail.com>
2017-04-14 09:34:46 +0200

    clean up unused files

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+rm SI*.xsl
+rm *.html
 ./convert_linux.sh SI-UBL-PO.SCH  SI-UBL-PO-1.2.xsl
 ./convert_linux.sh SI-UBL-INV.SCH  SI-UBL-INV-1.2.xsl
 saxon-xslt -o rules-po-overview.html  SI-UBL-PO-1.2.xsl rules-overview.xsl 
