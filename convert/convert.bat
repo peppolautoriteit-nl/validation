e1ebaf1ce2b3ded2bd80663547e91024adda9d93
Jelte Jansen <github@tjeb.nl>
2022-04-15 16:42:37 +0200

    Fix issues with convert.bat. Fixes #36

--- convert/convert.bat
+++ convert/convert.bat
@@ -5,19 +5,20 @@
 @set SAX="%MYDIR%/lib/saxon9he.jar"
 @set CONVERT_XSL_PATH="%MYDIR%/convert_xsl"
 
-if not exist "%TMPDIR%" mkdir "%TMPDIR%"
+if not exist %TMPDIR% mkdir %TMPDIR%
 
-java -jar %SAX% -o:%TMPDIR%/step1_dsdl.xsl $INPUT_FILE %CONVERT_XSL_PATH%/iso_dsdl_include.xsl &&\
+java -jar %SAX% -o:%TMPDIR%/step1_dsdl.xsl %INPUT_FILE% %CONVERT_XSL_PATH%/iso_dsdl_include.xsl
 @if not %errorlevel%==0 goto error
-java -jar %SAX% -o:%TMPDIR%/step2_abstract.xsl %TMPDIR%/step1_dsdl.xsl %CONVERT_XSL_PATH%/iso_abstract_expand.xsl &&\
+java -jar %SAX% -o:%TMPDIR%/step2_abstract.xsl %TMPDIR%/step1_dsdl.xsl %CONVERT_XSL_PATH%/iso_abstract_expand.xsl
 @if not %errorlevel%==0 goto error
-java -jar %SAX% -o:%OUTPUT_FILE% %TMPDIR%/step2_abstract.xsl %CONVERT_XSL_PATH%/iso_svrl_for_xslt2.xsl &&\
+java -jar %SAX% -o:%OUTPUT_FILE% %TMPDIR%/step2_abstract.xsl %CONVERT_XSL_PATH%/iso_svrl_for_xslt2.xsl
 @if not %errorlevel%==0 goto error
 
-@echo Artefact validation XSLT written to %file_name%.xsl
+@echo Artefact validation XSLT written to %OUTPUT_FILE%
 @goto end
 
 :error
 @echo Error during conversion, output not written
 
 :end
+@rmdir /Q /S %TMPDIR%
